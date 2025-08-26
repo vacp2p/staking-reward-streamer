0fa101df140afd195d4157e20103363899624e1a
r4bbit <445106+0x-r4bbit@users.noreply.github.com>
2025-08-26 15:34:15 +0200

    fix(StakeVault): add `lockUntil` validity check
    In case the `StakeManager` is malicious and returns `lockUntil` values
    that don't make sense, we calculate the new `lockUntil` values in the
    `StakeVault` to compare against those.
    
    If the values coming from `StakeManager` are not matching, something's
    off and we revert.
    
    We still do `lockUntil` calculations in `StakeManager` via `StakeMath`
    to ensure absolute max MP isn't exceeded.
    
    We don't have the notion of multiplier points in the `StakeVault`, nor
    should it know about it. Hence, we're just doing the `lockUntil`
    calculation.
    
    **Note**
    
    This introduces another "vulnerability" that I consider "acknowledged",
    but won't fix:
    
    In theory, a malicious stake manager upgrade could remove the upper
    bound checks on absolute max MP. In such a case, users could pass a very
    large `lockUntil` to `StakeVault`. `StakeManager` would simply calculate
    the same `lockUntil` but wouldn't revert on the max cases.
    
    So in other words, users can hurt themselves if stake manager removed
    the check and users willingly lock for very long time.
    
    Closes #249

--- src/StakeVault.sol
+++ src/StakeVault.sol
@@ -7,6 +7,7 @@ import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/I
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IStakeManagerProxy } from "./interfaces/IStakeManagerProxy.sol";
 import { IStakeVault } from "./interfaces/IStakeVault.sol";
+import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
 
 /**
  * @title StakeVault
@@ -37,6 +38,8 @@ contract StakeVault is IStakeVault, Initializable, OwnableUpgradeable {
     error StakeVault__NotAuthorized();
     /// @notice Emitted when withdrawing funds from vault fails
     error StakeVault__WithdrawFromVaultFailed();
+    /// @notice Emitted when receiving invalid lock end timestamp from stake manager
+    error StakeVault__InvalidLockEnd();
 
     /*//////////////////////////////////////////////////////////////////////////
                                   STATE VARIABLES
@@ -128,7 +131,9 @@ contract StakeVault is IStakeVault, Initializable, OwnableUpgradeable {
      * @param _seconds The time period to lock the staked amount for.
      */
     function lock(uint256 _seconds) external onlyOwner {
-        lockUntil = stakeManager.lock(_seconds, lockUntil);
+        uint256 newLockUntil = stakeManager.lock(_seconds, lockUntil);
+        _ensureLockUntilValid(newLockUntil, _seconds);
+        lockUntil = newLockUntil;
     }
 
     /**
@@ -278,7 +283,9 @@ contract StakeVault is IStakeVault, Initializable, OwnableUpgradeable {
      * @param _source The address from which tokens will be transferred.
      */
     function _stake(uint256 _amount, uint256 _seconds, address _source) internal {
-        lockUntil = stakeManager.stake(_amount, _seconds, lockUntil);
+        uint256 newLockUntil = stakeManager.stake(_amount, _seconds, lockUntil);
+        _ensureLockUntilValid(newLockUntil, _seconds);
+        lockUntil = newLockUntil;
         bool success = STAKING_TOKEN.transferFrom(_source, address(this), _amount);
         if (!success) {
             revert StakeVault__StakingFailed();
@@ -315,6 +322,25 @@ contract StakeVault is IStakeVault, Initializable, OwnableUpgradeable {
             revert StakeVault__NotEnoughAvailableBalance();
         }
         _token.transfer(_destination, _amount);
+    }
+
+    /**
+     * @notice Ensures that the new lock until timestamp is valid.
+     * @dev Reverts if the new lock until timestamp is not as expected.
+     * @param _newLockUntil The new lock until timestamp.
+     * @param _seconds The time period to stake for.
+     */
+    function _ensureLockUntilValid(uint256 _newLockUntil, uint256 _seconds) internal {
+        uint256 expectedLockUntil = Math.max(lockUntil, block.timestamp) + _seconds;
+        if (_seconds > 0 && _newLockUntil != expectedLockUntil) {
+            // This should never happen, unless there was a malicious
+            // upgrade of stake manager which tries to lock funds for longer than expected.
+            // Also, there's no case where `newLockUntil < expectedLockUntil`.
+            // It should revert in `StakeManager` already.
+            // We still do a `!=` check to be extra safe, in case a stake manager upgrade
+            // removes all other checks.
+            revert StakeVault__InvalidLockEnd();
+        }
     }
 
     /**
